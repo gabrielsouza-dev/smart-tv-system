6bb6c15064f5375b82e86a7a358c4e56442281cd
gabrielsouza-dev <gabrieldasilvaesouza@gmail.com>
2023-01-27 18:39:23 -0300

    commit inicial

---
+++ src/SmartTv.java
@@ -0,0 +1,37 @@
+public class SmartTv {
+  
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+
+    public void ligar(){
+      ligada = true;
+    }
+
+    public void aumentarVolume(){
+      System.out.println("Aumentando o volume para: " + volume);
+      volume ++;
+    }
+
+    public void diminuirVolume(){
+      System.out.println("Diminuindo o volume para: " + volume);
+      volume --;
+    }
+  
+    public void aumentarCanal() {    
+      canal ++;
+    }
+
+    public void diminuirCanal() {
+      canal --;
+    }
+
+    public void mudarCanal(int novoCanal){
+      canal = novoCanal;
+    }
+
+    public void desligar(){
+      ligada = false;
+    }
+
+}
---
+++ src/Usuario.java
@@ -0,0 +1,28 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+      SmartTv smartTv = new SmartTv();
+
+      System.out.println("TV Ligada? " + smartTv.ligada);
+      System.out.println("Canal atual: " + smartTv.canal);
+      System.out.println("Volume atual: " + smartTv.volume);
+
+      smartTv.ligar();
+      System.out.println("Novo Status -> TV Ligada? " + smartTv.ligada);
+
+      smartTv.diminuirVolume();
+      smartTv.diminuirVolume();
+      smartTv.diminuirVolume();
+      smartTv.aumentarVolume();
+
+      System.out.println("Volume atual: " + smartTv.volume);
+
+      System.out.println("Canal atual: " + smartTv.canal);
+
+      smartTv.mudarCanal(13);
+      
+      System.out.println("Canal atual: " + smartTv.canal);
+
+      smartTv.desligar();
+      System.out.println("Novo Status -> TV Ligada? " + smartTv.ligada);
+    }
+}
